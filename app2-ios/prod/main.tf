c7310e34550d7568814b1300bd04fcde0d0d1419
Rafa <rafa@portillo.eu>
2022-08-24 20:37:12 +0200

    remove commented tf code

--- app2-ios/prod/main.tf
+++ app2-ios/prod/main.tf
@@ -31,20 +31,3 @@ module "static_website" {
   static_website_error_404_document = "error.html"
   module_tags                       = local.tags
 }
-
-# # Second module "call"
-# module "second_static_website" {
-#   source = "../../modules/azure-static-website"
-
-#   # Resource Group
-#   region  = "westeurope"
-#   rg_name = "modreg2"
-
-#   # Storage Account
-#   storage_account_name              = "staticwebsite"
-#   storage_account_tier              = "Standard"
-#   storage_account_replication_type  = "LRS"
-#   storage_account_kind              = "StorageV2"
-#   static_website_index_document     = "index.html"
-#   static_website_error_404_document = "error.html"
-# }
